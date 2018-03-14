89ff9b5e3982af88334f2eba3acc7737d85f5c5d
Duncan Chiang <duncan.c@inwinstack.com>
2018-03-14 17:11:09 +0800

    [Feature] Hide relogin button.

--- templates/userdisable.php
+++ templates/userdisable.php
@@ -5,6 +5,6 @@ ul>
 	<li class='update'>
         <?php p($l->t('Your account is disabled now.')); ?><br/><br/>
         <?php p($l->t('Please contact your system administrator if this message appeared.')) ?><br/><br/>
-        <a class="button" <?php print_unescaped(OC_User::getLogoutAttribute()); ?>><?php p($l->t("Redirect to login page")) ?></a>
+        <!--<a class="button" <?php print_unescaped(OC_User::getLogoutAttribute()); ?>><?php p($l->t("Redirect to login page")) ?></a>-->
 	</li>
 </ul>
